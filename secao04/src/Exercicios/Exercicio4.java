f1886db25f1f78deb06cdfe1224061e2ea0444dc
Luana Otsuka <otsuka.luana@gmail.com>
2021-07-07 10:50:48 -0300

    Section 4 - exercise 4 completed

---
+++ secao04/src/Exercicios/Exercicio4.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		
+		int numFuncionario, horasTrabalhadas;
+		double valorPorHora, salario;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		numFuncionario = sc.nextInt();
+		horasTrabalhadas = sc.nextInt();
+		valorPorHora = sc.nextDouble();
+		
+		salario = horasTrabalhadas * valorPorHora;
+		
+		System.out.println("NUMBER: " + numFuncionario);
+		System.out.printf("SALARY: U$ %.2f", salario);
+		
+		sc.close();
+		
+	}
+}
