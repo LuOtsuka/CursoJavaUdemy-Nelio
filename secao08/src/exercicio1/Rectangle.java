e95ceb449ed642e5ac505095b966a30a89eced99
Luana Otsuka <otsuka.luana@gmail.com>
2021-07-19 09:55:18 -0300

    Section 8 - exercise 1 completed

---
+++ secao08/src/exercicio1/Rectangle.java
@@ -0,0 +1,25 @@
+package exercicio1;
+
+public class Rectangle {
+	
+	double width;
+	double height;
+	
+	public double area() {
+		
+		return width * height;
+		
+	}
+	
+	public double perimeter() {
+		
+		return (height * 2) + (width * 2);
+		
+	}
+	
+	public double diagonal() {
+		double diagonal = Math.sqrt((width * width) + (height * height));
+		return diagonal;
+	}
+
+}
---
+++ secao08/src/exercicio1/program.java
@@ -0,0 +1,24 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Rectangle rectangle = new Rectangle();
+		
+		System.out.println("Enter a rectangle width and height: ");
+		rectangle.width = scan.nextDouble();
+		rectangle.height = scan.nextDouble();
+		
+		System.out.printf("AREA: %.2f%n", rectangle.area());
+		System.out.printf("PERIMETER: %.2f%n", rectangle.perimeter());
+		System.out.printf("DIAGONAL: %.2f%n", rectangle.diagonal());
+		
+		scan.close();
+
+	}
+
+}
